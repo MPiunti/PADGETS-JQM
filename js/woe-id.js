56e7c832630c4c0b6a86d63ae788f2b5f766ff58
Michele Piunti <m.piunti@reply.it>
2012-05-17 17:48:31 +0200

    Where On Earth navigation

--- js/woe-id.js
+++ js/woe-id.js
@@ -1,30 +1,51 @@
-//var serviceURL = "http://localhost/directory/services/";
-
-var serviceURL = "http://where.yahooapis.com/v1/";
-
+var APIURL = "http://where.yahooapis.com/v1/";
 var appId = "CQ4A0PrV34HySShcdReYzp07vLl6ZmyYeZfC3QKLr8nNP8WsAWFZVBcSjb1C4YJFP3HawM.g5zrE._eRDKmc2UI4anv7uxo-";
-var queryPlace = "Rom";
 
-var lois;
+var queryPlace, places, serviceURL; 
 
-$('#woePage').bind('pageinit', function(event) {
+
+
+
+$('#woePage').live('pageshow', function(event) {
+	queryPlace = getUrlVars()["name"];
+	console.log("query for: " + queryPlace );
+	serviceURL = APIURL + 'places.q(\''+ queryPlace  +'\');start=0;count=18?appid=' + appId;
 	getWoE();
 });
 
 function getWoE() {
-	$.getJSON(serviceURL + 'places.q(\''+ queryPlace  +'\')?appid=' + appId, function(data) {
+	$.getJSON( serviceURL , function(data) {
 		$('#woeList li').remove();
-		lois = data.places;
-		console.log('places: '+ lois);
-		$.each(lois, function(index, item) {
-			console.log(item);
-			$('#woeList').append('<li>'+
-			'<h4>' + item[0].country + '  ' + item[0].locality1 + '</h4>' +
-				'<a href="/?id=' + item[0].name + '">' +
-					'<h4>Where On Earth Id: ' + item[0].woeid + '</h4>' +					
-					'<p>' + item[0].admin1 + '</p>'+
-					'<span class="ui-li-count">' + item[0].woeid + '</span> </a></li>' );
+		places = data.places;
+		console.log('places n: '+ places.count);
+		$.each(places, function(index, item) {
+			//console.log('item:' + item);
+			
+			$.each(item, function(index2, place) {
+				//console.log('places:' + item);
+				if(place!= null){
+				$('#woeList').append('<li>'+
+				'<h4>' + place.country + ' - ' + place.name + '</h4>' +
+				'<h4>Where On Earth Id: ' + place.woeid + '</h4>' +					
+					'<p>' + place.locality1 + '  ' + place.admin1 + '</p>'+
+					'<a href="detail.html?woeid=' + place.woeid + '">' +
+					'<span class="ui-li-count">' + place.woeid + '</span> </a></li>' );
+				}
+			});
 		});
 		$('#woeList').listview('refresh');
 	});
+}
+
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
 }
